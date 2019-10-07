895e0f57a7d1f07426c3ad95e5745c5887f5ad36
ezavod <ezavod@gmx.de>
2019-10-07 18:47:42 +0200

    fix(TRR): typo in the routine that writes the frame header
    This prevents one from writing velocities or forces without position data.

--- src/xdrfile_trr.c
+++ src/xdrfile_trr.c
@@ -271,7 +271,7 @@ static int do_htrn(XDRFILE* xd, mybool bRead, t_trnheader* sh, matrix box,
             if (!bRead) {
                 for (i = 0; (i < sh->natoms); i++) {
                     for (j = 0; (j < DIM); j++) {
-                        if (NULL != x) {
+                        if (NULL != v) {
                             dx[i * DIM + j] = (double)v[i][j];
                         }
                     }
@@ -294,7 +294,7 @@ static int do_htrn(XDRFILE* xd, mybool bRead, t_trnheader* sh, matrix box,
             if (!bRead) {
                 for (i = 0; (i < sh->natoms); i++) {
                     for (j = 0; (j < DIM); j++) {
-                        if (NULL != x) {
+                        if (NULL != f) {
                             dx[i * DIM + j] = (double)f[i][j];
                         }
                     }
@@ -389,7 +389,7 @@ static int do_htrn(XDRFILE* xd, mybool bRead, t_trnheader* sh, matrix box,
             if (!bRead) {
                 for (i = 0; (i < sh->natoms); i++) {
                     for (j = 0; (j < DIM); j++) {
-                        if (NULL != x) {
+                        if (NULL != v) {
                             fx[i * DIM + j] = v[i][j];
                         }
                     }
@@ -412,7 +412,7 @@ static int do_htrn(XDRFILE* xd, mybool bRead, t_trnheader* sh, matrix box,
             if (!bRead) {
                 for (i = 0; (i < sh->natoms); i++) {
                     for (j = 0; (j < DIM); j++) {
-                        if (NULL != x) {
+                        if (NULL != f) {
                             fx[i * DIM + j] = f[i][j];
                         }
                     }
